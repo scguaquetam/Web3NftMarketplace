efd87a6d895d6a5d423cd023e9c46b5741f8c014
Sebas <sheva3232@gmail.com>
2023-03-21 08:14:32 -0500

    adding packages

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { HashRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </HashRouter>
   </React.StrictMode>
 );
 
